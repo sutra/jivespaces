f9bbda68f718a357088ee48c7a7e84e407690394
Rory.CN <Rory.CN@475f77a9-321b-0410-8973-87d19a86b84c>
2006-08-21 16:12:15 +0000

    add page find method

--- jivespaces/src/main/java/com/jivespaces/dao/hibernate3/Hibernate3EntryDao.java
+++ jivespaces/src/main/java/com/jivespaces/dao/hibernate3/Hibernate3EntryDao.java
@@ -3,8 +3,16 @@
  */
 package com.jivespaces.dao.hibernate3;
 
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.util.Assert;
+
 import com.jivespaces.dao.EntryDao;
 import com.jivespaces.domain.Entry;
+import com.jivespaces.util.PaginationSupport;
 
 /**
  * @author Shutra
@@ -22,4 +30,26 @@ public class Hibernate3EntryDao extends BaseHibernateEntityDao<Entry> implements
 		return get(id);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.jivespaces.dao.EntryDao#getEntryList(com.jivespaces.domain.Entry, int, int)
+	 */
+	public PaginationSupport getEntryList(Entry entry, int pageSize, int startIndex) {
+		Assert.notNull(entry, "Search is required!");
+		DetachedCriteria criteria = DetachedCriteria.forClass(Entry.class);
+		if (StringUtils.isNotBlank(entry.getTitle())) {
+			criteria.add(Restrictions.ilike("title", entry.getTitle(), MatchMode.ANYWHERE));
+		}
+		return findPageByCriteria(criteria, pageSize, startIndex);
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.jivespaces.dao.EntryDao#getEntryListByTag(java.lang.String, int, int)
+	 */
+	public PaginationSupport getEntryListByTag(String tagName, int pageSize, int startIndex) {
+		Assert.notNull(tagName, "search is required");
+		DetachedCriteria criteria = DetachedCriteria.forClass(Entry.class);
+		criteria.addOrder(Order.desc(""));
+		criteria.createCriteria("tags").add(Restrictions.eq("name", tagName));
+		return findPageByCriteria(criteria, pageSize, startIndex);
+	}
 }
