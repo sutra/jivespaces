611db571fdb8825079be8ab62b531c6a344162b7
Rory.CN <Rory.CN@475f77a9-321b-0410-8973-87d19a86b84c>
2006-08-21 16:12:15 +0000

    add page find method

--- trunk/jivespaces/src/main/java/com/jivespaces/dao/EntryDao.java
+++ trunk/jivespaces/src/main/java/com/jivespaces/dao/EntryDao.java
@@ -4,6 +4,7 @@
 package com.jivespaces.dao;
 
 import com.jivespaces.domain.Entry;
+import com.jivespaces.util.PaginationSupport;
 
 /**
  * @author Shutra
@@ -11,4 +12,26 @@ import com.jivespaces.domain.Entry;
  */
 public interface EntryDao {
 	Entry getEntry(String id);
+	
+	/**
+	 * 分页查找Entry
+	 * @param entry 查询条件
+	 * @param pageSize 分页大小
+	 * @param startIndex 记录开始位置
+	 * @return 分页工具类
+	 * @see PaginationSupport
+	 * @author <a href="mailto:rory.cn@gmail.com">somebody</a>
+	 */
+	public PaginationSupport getEntryList(Entry entry, int pageSize, int startIndex);
+	
+	/**
+	 * 根据TagName查找Entry
+	 * @param tagName Tag名称
+	 * @param pageSize 分页大小
+	 * @param startIndex 记录开始位置
+	 * @return 分页工具类
+	 * @see PaginationSupport
+	 * @author <a href="mailto:rory.cn@gmail.com">somebody</a>
+	 */
+	public PaginationSupport getEntryListByTag(String tagName, int pageSize, int startIndex);
 }
